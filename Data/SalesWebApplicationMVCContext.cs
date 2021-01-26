826a52b222e722adb873a75e277e2bfa665514aa
Thiago Mello <tgvm96@gmail.com>
2021-01-26 14:21:42 -0300

    CRUD Scaffolding

---
+++ Data/SalesWebApplicationMVCContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace SalesWebApplicationMVC.Models
+{
+    public class SalesWebApplicationMVCContext : DbContext
+    {
+        public SalesWebApplicationMVCContext (DbContextOptions<SalesWebApplicationMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SalesWebApplicationMVC.Models.Department> Department { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -9,6 +9,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesWebApplicationMVC.Models;
 
 namespace SalesWebApplicationMVC
 {
@@ -33,6 +35,9 @@ namespace SalesWebApplicationMVC
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<SalesWebApplicationMVCContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("SalesWebApplicationMVCContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
