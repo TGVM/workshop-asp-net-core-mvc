781914e470d1d783d48e699dbe73720b16b0e7b4
Thiago Mello <tgvm96@gmail.com>
2021-01-26 15:32:36 -0300

    Outras classes

--- Migrations/20210126183115_OtherEntities.Designer.cs
+++ Migrations/20210126183115_OtherEntities.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SalesWebApplicationMVC.Models;
 
 namespace SalesWebApplicationMVC.Migrations
 {
     [DbContext(typeof(SalesWebApplicationMVCContext))]
-    partial class SalesWebApplicationMVCContextModelSnapshot : ModelSnapshot
+    [Migration("20210126183115_OtherEntities")]
+    partial class OtherEntities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
