0bab3842639d7f467d9df8d855ed1f070f0312d4
Thiago Mello <tgvm96@gmail.com>
2021-01-27 15:29:12 -0300

    Preparando telas de pesquisa

---
+++ Services/Exceptions/IntegrityException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace SalesWebApplicationMVC.Services.Exceptions
+{
+    public class IntegrityException : ApplicationException
+    {
+        public IntegrityException(string message) : base(message) { 
+        }
+
+    }
+}
